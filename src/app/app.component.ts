8f213fafcb28dcd8d8f9305b6a7a4536e8227e8e
Alexis Gonzalez <1991.alexis@gmail.com>
2024-01-16 11:01:50 -0400

    Installed and tried to use ngx-capture but got error TS2707

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgxCaptureService } from 'ngx-capture';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'screenshot-second-chance';
+  @ViewChild('area-to-print', { static: true }) areaToPrint: any;
+
+  constructor(private captureService: NgxCaptureService) {}
+
   shouldDownload(html2canvas: boolean) {
     if (html2canvas) {
+      console.log('using html to canvas');
     } else {
+      console.log('using ngx-capture');
+      this.captureService
+        .getImage(this.areaToPrint.nativeElement, true)
+        .pipe(
+          tap((img) => {
+            console.log(img);
+          })
+        )
+        .subscribe();
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { NgxCaptureModule } from 'ngx-capture';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
+    NgxCaptureModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
